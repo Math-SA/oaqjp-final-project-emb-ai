e04776f828ffabb937b1679499ee1c17a7880b7b
Math-SA <matheus.s.assis@gmail.com>
2025-01-11 08:21:31 -0500

    Step 6: added a flask server for UI

---
+++ server.py
@@ -0,0 +1,28 @@
+''' Flask server to provide an UI for the Emotion Detector app '''
+from flask import Flask, request, render_template
+from EmotionDetection import emotion_detector
+
+app = Flask("Emotion Detector app. Powered by IBM Watson NLP Service")
+
+#Routes
+@app.route("/emotionDetector")
+def analyze():
+    #get parameter textToAnalyze and analyze
+    emotion = emotion_detector(request.args.get('textToAnalyze'))
+    response = f"For the given statement, the system response is \
+        'anger': {emotion['anger']},\
+        'disgust': {emotion['disgust']},\
+        'fear': {emotion['fear']}, \
+        'joy': {emotion['joy']} and \
+        'sadness': {emotion['sadness']}. \
+        The dominant emotion is {emotion['dominant_emotion']}."
+    return response
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+    
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
