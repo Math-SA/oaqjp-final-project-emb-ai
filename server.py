63887def1d66da2d1c1202204c1e507b1441cf50
Math-SA <matheus.s.assis@gmail.com>
2025-01-11 09:26:02 -0500

    Task 8: Pylint server.py

--- server.py
+++ server.py
@@ -7,10 +7,11 @@ app = Flask("Emotion Detector app. Powered by IBM Watson NLP Service")
 #Routes
 @app.route("/emotionDetector")
 def analyze():
+    ''' Analyzes a given text and formats the output as specified in the instructions'''
     #get parameter textToAnalyze and analyze
     emotion = emotion_detector(request.args.get('textToAnalyze'))
-    if (emotion['dominant_emotion'] == None):
-         return '<b>Invalid text! Please try again!</b>'
+    if emotion['dominant_emotion'] is None:
+        return '<b>Invalid text! Please try again!</b>'
 
     response = f"For the given statement, the system response is \
         'anger': {emotion['anger']},\
@@ -23,9 +24,8 @@ def analyze():
 
 @app.route("/")
 def index():
+    ''' Renders the main page '''
     return render_template('index.html')
-    
-
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
